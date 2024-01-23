6cc8eb4e4d0ea7c1e3ff4f11f5660913a60a2e43
Olle de Jong <olledejong@gmail.com>
2024-01-23 10:44:16 +0100

    Added helper function for finding all .edf files in directory (and subdirectories)

---
+++ helper_functions.py
@@ -0,0 +1,18 @@
+"""
+File that contains helper functions
+"""
+import re
+import os
+import glob
+import numpy as np
+import pandas as pd
+
+
+def get_all_edf_files(root_dir):
+    # find all .edf files (also works if all .edf files are in the root directory)
+    edf_files = []
+    for root, dirs, files in os.walk(root_dir):
+        if "trash_recordings" in dirs:  # do not handle recordings that are in trash folder
+            dirs.remove("trash_recordings")
+        edf_files.extend(glob.glob(os.path.join(root, '*.edf')))
+    return edf_files
