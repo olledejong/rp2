d10ef0363525b4ca2351d3f68f447cef440136ee
Olle de Jong <olledejong@gmail.com>
2024-02-01 11:53:30 +0100

    Added comment

--- psd_calculations/psd_of_fixed_epoch.py
+++ psd_calculations/psd_of_fixed_epoch.py
@@ -14,6 +14,7 @@ import pandas as pd
 from pynwb import NWBHDF5IO
 from mne.time_frequency import psd_array_multitaper
 
+# Script starts here
 if __name__ == '__main__':
     # load settings
     with open('../settings.json', "r") as f:
