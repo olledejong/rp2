2bf3e3fd787f0c808d9bb08d41f67877b8c14851
Olle de Jong <olledejong@gmail.com>
2024-02-06 15:17:25 +0100

    Logic for extracting the state of the LED in the movies (later used to align EEG and subject movement data)

---
+++ video_analysis/identify_led_rois.py
@@ -0,0 +1,104 @@
+import os
+import cv2
+import pandas as pd
+from tkinter import filedialog
+from tkinter import Tk
+
+
+def get_folder_path():
+    """
+    Allows user to identify the folder that holds the video files to be analyzed
+
+    :return: path to selected folder
+    """
+    root = Tk()
+    root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    folder_selected = filedialog.askdirectory()  # show an "Open" dialog box and return the path to the selected folder
+    return folder_selected
+
+
+def draw_roi(frame, roi):
+    """
+    Draws a ROI in the given frame.
+
+    :param frame:
+    :param roi:
+    :return:
+    """
+    x, y, w, h = roi
+    cv2.rectangle(frame, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+
+
+def select_roi(frame):
+    """
+    Returns from the frame selected ROI.
+
+    :param frame:
+    :return:
+    """
+    r = cv2.selectROI("Select LED Area", frame, fromCenter=False, showCrosshair=True)
+    cv2.destroyAllWindows()
+    return r
+
+
+def select_new_roi(frame, old_roi):
+    """
+    Function that allows user to select a new ROI for the new video. If enter is pressed
+    without a new selection, then the old roi is kept.
+
+    :param frame:
+    :param old_roi:
+    :return:
+    """
+    r = cv2.selectROI("Select new LED Area or press enter to keep the pre-selection", frame, fromCenter=False,
+                      showCrosshair=True)
+    cv2.destroyAllWindows()
+    if r == (0, 0, 0, 0):  # if nothing is selected
+        return old_roi
+    return r
+
+
+def save_led_rois(folder_path):
+    """
+    Loops through video files in the folder selected through Tkinter and lets the user
+    select a ROI. Each video results in an entry into a dataframe, which is eventually
+    saved to the filesystem.
+
+    :param folder_path: path to the selected folder (Tkinter)
+    :return:
+    """
+    videos = [f for f in os.listdir(folder_path) if f.endswith(('.mp4', '.avi'))]
+    roi = None
+    data = []
+
+    for video in videos:
+        video_path = os.path.join(folder_path, video)
+        # read first frame (we only need one for determining ROI)
+        cap = cv2.VideoCapture(video_path)
+        ret, frame = cap.read()
+
+        if not ret:  # when incapable of reading frame
+            print(f"Failed to read video: {video}")
+            continue
+
+        if roi is None:  # first video in dir; there's known roi yet
+            roi = select_roi(frame)
+            draw_roi(frame, roi)
+        elif roi is not None:  # if not first selected roi; show previously selected roi
+            draw_roi(frame, roi)
+
+        # allow user to select new roi when previous one is not correct
+        roi = select_new_roi(frame, roi)
+
+        # save the data
+        data.append({'Video': video, 'ROI': roi})
+        cap.release()
+        cv2.destroyAllWindows()
+
+    df = pd.DataFrame(data)
+    df.to_excel(os.path.join(folder_path, 'video_rois.xlsx'), index=False)
+
+
+if __name__ == "__main__":
+    video_folder_path = get_folder_path()
+    save_led_rois(video_folder_path)
---
+++ video_analysis/ttl_onset_extractor.py
@@ -0,0 +1,98 @@
+import os
+import ast
+import cv2
+import pickle
+import pandas as pd
+import numpy as np
+
+
+def is_led_on(roi, threshold=245):
+    """
+    Returns True when the LED is on, False otherwise
+
+    :param roi:
+    :param threshold:
+    :return:
+    """
+    return np.max(roi) >= threshold
+
+
+def export_frame(video_name, frame_number, frame, state, images_path):
+    """
+    Exports a snapshot/frame to a PNG file for validation purposes.
+
+    :param video_name:
+    :param frame_number:
+    :param frame:
+    :param state:
+    :param images_path:
+    :return:
+    """
+    filename = f"{os.path.splitext(video_name)[0]}_frame{frame_number}_{state}.png"
+    save_to = f"{images_path}/{filename}"
+    cv2.imwrite(save_to, frame)
+
+
+def get_led_states(rois_df, vid_folder_path, snapshot_path):
+    """
+    For every row in the rois_df (holds the movie filename and roi info), the LED
+    state is extracted and is stored. All saved info is later saved to a file.
+
+    :param rois_df: every row had a filename (mp4/avi) and a ROI
+    :param vid_folder_path: path to where the videos are located
+    :param snapshot_path: path to where snapshots (for validation) are stored
+    :return:
+    """
+    all_led_states = {}
+
+    for index, row in rois_df.iterrows():
+        print(f"Working with video {row['Video']}.")
+        video_path = os.path.join(vid_folder_path, row['Video'])
+        roi = ast.literal_eval(row['ROI'])
+        cap = cv2.VideoCapture(video_path)
+
+        frame_number, prev_frame, prev_state = 0, None, 0
+
+        states = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:  # if not successfully read, stop iterating frames of this video
+                break
+
+            x, y, w, h = roi  # unpack tuple object
+            roi_frame = frame[y:y + h, x:x + w]  # slice the roi data from the entire frame
+            state = is_led_on(roi_frame)  # check if on or off
+
+            # export frames when LED is on ór when the state changes
+            if state or (state != prev_state):
+                export_frame(row['Video'], frame_number, frame, 'ON' if state else 'OFF', snapshot_path)
+                # if there is a ON state, of if there is a change in state, and if there is a previously evaluated
+                # frame: also export the previous frame. This ensures the OFF frame before first ON frame is also saved.
+                if prev_frame is not None:
+                    export_frame(row['Video'], frame_number - 1, prev_frame, 'ON' if prev_state else 'OFF', snapshot_path)
+
+            prev_frame = frame.copy()  # save frame for next iteration
+            prev_state = state  # update state
+            states.append(int(state))  # save state
+            frame_number += 1  # increment frame counter
+
+        # close video and save led states
+        cap.release()
+        all_led_states[row['Video']] = np.array(states)
+
+    return all_led_states
+
+
+# process starts here
+if __name__ == '__main__':
+    roi_df_path = ""
+    folder_path = ""
+    images_path = ""
+    pickle_path = ""
+
+    roi_df = pd.read_excel(roi_df_path)  # read the roi dataframe created with the identify_led_rois.py script
+    led_states = get_led_states(roi_df, folder_path, images_path)  # get the LED states for all frames of every file
+
+    # save the LED states for every frame of every video file
+    with open(f'{pickle_path}/led_states.pickle', "wb") as f:
+        pickle.dump(led_states, f, pickle.HIGHEST_PROTOCOL)
