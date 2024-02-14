21c026cfc42c41b6af98b06b5a104d2f0fbcb1db
Olle de Jong <olledejong@gmail.com>
2024-02-14 11:39:00 +0100

    Added function to save matplotlib figures

--- helper_functions.py
+++ helper_functions.py
@@ -3,6 +3,7 @@ File that contains helper functions
 """
 import os
 import glob
+import matplotlib.pyplot as plt
 
 
 def get_all_edf_files(root_dir):
@@ -13,3 +14,22 @@ def get_all_edf_files(root_dir):
             dirs.remove("trash_recordings")
         edf_files.extend(glob.glob(os.path.join(root, '*.edf')))
     return edf_files
+
+
+def save_figure(path, bbox_inches='tight', dpi=300):
+    """
+    Custom function that lets you save a pyplot figure and creates the directory where necessary
+    """
+    directory = os.path.split(path)[0]
+    filename = os.path.split(path)[1]
+    if directory == '':
+        directory = '.'
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    save_path = os.path.join(directory, filename)
+
+    # Actually save the figure
+    plt.savefig(save_path, bbox_inches=bbox_inches, dpi=dpi)
+    plt.close()
