b32a629f416e95c164262c6a682a3a8f938d7ed7
Olle de Jong <olledejong@gmail.com>
2024-02-22 15:17:50 +0100

    EEG sample --> Frame number conversion logic has moved from this script to eeg_epoching.py. File is altered because of that.

--- epoching/add_movement_data_to_epochs.py
+++ epoching/add_movement_data_to_epochs.py
@@ -7,48 +7,9 @@ epoch or not.
 import os
 import mne
 import json
-import pickle
 import ndx_events
 import numpy as np
-import pandas as pd
 from pynwb import NWBHDF5IO
-
-
-def get_settings(settings_path):
-    with open(settings_path, "r") as f:
-        return json.load(f)
-
-
-def frame_to_sample(frame_number, video_fps, offset):
-    """
-    Function that calculates the time-point of the video (in frames) given
-     the sample number in the EEG.
-    """
-    tp_secs_video = frame_number / video_fps  # time-point on video in seconds
-    secs_eeg = tp_secs_video + offset
-
-    return secs_eeg
-
-
-def sample_to_frame(eeg_sample, video_fps, s_freq, offset):
-    """
-    Function that calculates the time-point of the video (in frames) given
-     the sample number in the EEG.
-    """
-    eeg_tp_secs = eeg_sample / s_freq  # from samples to seconds
-    video_tp_secs = eeg_tp_secs - offset  # subtract the offset so we have the video tp in secs
-
-    return video_tp_secs * video_fps  # go to frames
-
-
-def get_led_onset_file(pickle_path):
-    with open(f'{pickle_path}/led_states_all_videos.pickle', "rb") as f:
-        return pickle.load(f)
-
-
-def get_subject_metadata(metadata_path, mouse_id):
-    metadata_df = pd.read_excel(metadata_path)
-    return metadata_df[metadata_df['mouseId'] == mouse_id]
 
 
 def calculate_offset(eeg_onsets, led_onsets, s_freq, video_fps):
@@ -88,47 +49,24 @@ def get_epoch_array(subject_id, epochs_folder):
             return epochs_array
 
 
-def get_led_onset_data(video_folder, movie_filename):
-    """
-    Loads the correct LED onset data from the pickle file which holds the LED
-    onset data for all experiment videos.
-
-    :param video_folder:
-    :param movie_filename:
-    :return:
-    """
-    print(f"Retrieved accompanying LED state data from file {movie_filename}")
-    with open(f'{video_folder}/pickle/led_states_all_videos.pickle', "rb") as f:
-        led_states = pickle.load(f)
-        led_states = led_states[movie_filename]
-        return led_states
-
-
-def get_movement_per_epoch(epochs_array, first_onset_offset, fps, movement_data, non_movement_cutoff, s_freq):
+def get_movement_per_epoch(epochs_array, movement_data, non_movement_cutoff):
     """
     Checks whether the subject is moving during each epoch and this information is stored
     in an array. This array is eventually saved within the metadata of the subject's epoch object
 
     :param epochs_array:
-    :param first_onset_offset:
-    :param fps:
     :param movement_data:
     :param non_movement_cutoff:
-    :param s_freq:
     :return:
     """
     # add new column to metadata holding the movement boolean value (initially we set them all to 'moving'
     movement_col = np.ones(len(epochs_array.metadata), dtype=bool)
     # set the new column to false for the epochs where there's no movement
     for i, epoch in enumerate(epochs_array):
-        epoch_start, epoch_end = epochs_array.metadata.iloc[i]["epochs_start_end"].split("-")
-
-        # convert start end end-point of epoch from samples to frames
-        frame_start = sample_to_frame(int(epoch_start), fps, s_freq, first_onset_offset)
-        frame_end = sample_to_frame(int(epoch_end), fps, s_freq, first_onset_offset)
+        epoch_start_frame, epoch_end_frame = epochs_array.metadata.iloc[i]["epochs_start_end_frames"].split("-")
 
         # use the start and end in frames to get the accompanying movement data
-        frame_start, frame_end = int(np.floor(frame_start)), int(np.ceil(frame_end))
+        frame_start, frame_end = int(np.floor(epoch_start_frame)), int(np.ceil(epoch_end_frame))
 
         # if there's no movement in this epoch, update the dataframe
         if np.sum(movement_data[frame_start:frame_end]) == non_movement_cutoff:
@@ -142,12 +80,11 @@ def main():
     all epochs and saves this to the filesystem.
     :return:
     """
-    fps = 30  # frames-per-second of video
     resting_cutoff = 0  # number of frames of movement that is allowed in one epoch
 
-    settings = get_settings("../settings.json")
-    nwb_folder, metadata_path = settings["nwb_files_folder"], settings["metadata"]
-    epochs_folder, video_folder = settings["epochs_folder"], settings["video_folder"]
+    with open("../settings.json", "r") as f:
+        settings = json.load(f)
+    nwb_folder, epochs_folder = settings["nwb_files_folder"], settings["epochs_folder"]
 
     all_epochs = []
     # loop through the nwb files (1 for each subject)
@@ -161,8 +98,6 @@ def main():
             nwb = io.read()
             subject_id = nwb.subject.subject_id
             genotype = nwb.subject.genotype
-            eeg_ttl_onsets_secs = list(nwb.acquisition["TTL_1"].timestamps)
-            sfreq = nwb.acquisition['raw_EEG'].rate
 
             try:  # not all nwb files have movement data
                 movement_data = nwb.processing["coordinate_data"]["motion"].data[:]
@@ -174,24 +109,20 @@ def main():
 
         # load the accompanying filtered epochs file
         epochs_array = get_epoch_array(subject_id, epochs_folder)
-
-        # get the subject's metadata from the file (holds video filename that points to right LED states)
-        subject_metadata = get_subject_metadata(metadata_path, int(subject_id))
-        movie_filename = subject_metadata["movie_filename"].iloc[0]  # movie fiilename the subject's in
-
-        # get the LED states for this subject (i.e. get the LED states of the correct video)
-        led_states = get_led_onset_data(video_folder, movie_filename)
-        # calculate the delta (offset) between the start and the first ttl onset for both datastreams
-        first_onset_offset = calculate_offset(eeg_ttl_onsets_secs, led_states, sfreq, fps)
         # generate the column that holds the movement data
-        movement_col = get_movement_per_epoch(epochs_array, first_onset_offset, fps, movement_data, resting_cutoff, sfreq)
+        movement_col = get_movement_per_epoch(epochs_array, movement_data, resting_cutoff)
 
-        epochs_array.metadata["movement"] = movement_col  # save the movement column to the subject's metadata
+        # save the movement column to the subject's metadata
+        epochs_array.metadata["movement"] = movement_col
         all_epochs.append(epochs_array)
 
+        epochs_array.save(os.path.join(epochs_folder, f"filtered_epochs_w_movement_{subject_id}-epo.fif"))
+
+    print("Saved epochs of each subject in individual file, now attempting to concatenate them as well..")
     # concatenate and save all epochs
     concatenated_epochs = mne.concatenate_epochs(all_epochs, add_offset=True)
     concatenated_epochs.save(os.path.join(epochs_folder, "filtered_epochs_w_movement-epo.fif"))
+    print("Done, bye.")
 
 
 # script starts here
