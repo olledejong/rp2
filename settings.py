da515706cb1a618a0fb960d28038444dcb8c0519
Olle de Jong <olledejong@gmail.com>
2024-03-14 15:14:55 +0100

    Added some constants to the settings file that can be reused

--- settings.py
+++ settings.py
@@ -19,6 +19,7 @@ paths = {
  "video_folder": ""
 }
 
+# variables used for raw EEG filtering while creating Neurodata Without Border (NWB) files (one of the first steps)
 filtering = {
     "lcut": 0.5,
     "hcut": 200,
@@ -36,4 +37,31 @@ filtering = {
         "EEG 7": ["EMG_L", 0, 0, 0, "emg"],
         "EEG 10": ["EMG_R", 0, 0, 0, "emg"]
     }
+}
+
+# EMG frequency bands used for resting-state epoch classification (interpretation not comparable to EEG bands)
+freq_bands_emg = {
+    'band1': (0, 10), 'band2': (10, 20),
+    'band3': (20, 30), 'band4': (30, 40),
+    'band5': (40, 50), 'band6': (50, 60),
+    'band7': (60, 70), 'band8': (70, 80),
+    'band9': (80, 90), 'band10': (90, 100)
+}
+
+# EEG frequency bands used throughout the whole project for Power Spectral Density analysis
+freq_bands_eeg = {
+    r'$\delta$': (1, 4),  # Delta
+    r'$\theta$': (4, 8),  # Theta
+    r'$\alpha$': (8, 13),  # Alpha
+    r'$\beta$': (13, 30),  # Beta
+    r'$\gamma$': (30, 100)  # Gamma
+}
+
+# dictionary holding the channel names of the EMGs for all subject of which the quality is high enough for analysis
+quality_emgs = {
+    81217: ["EMG_L"], 81175: ["EMG_L", "EMG_R"], 79592: ["EMG_R"], 79593: ["EMG_L"],
+    81207: ["EMG_R"], 80625: ["EMG_R"], 80630: ["EMG_L", "EMG_R"], 78211: ["EMG_R"],
+    39489: ["EMG_L"], 80620: ["EMG_L", "EMG_R"], 78227: ["EMG_L", "EMG_R"], 78233: ["EMG_R"],
+    39508: ["EMG_L", "EMG_R"], 79604: ["EMG_L", "EMG_R"], 81218: ["EMG_L", "EMG_R"],
+    79602: ["EMG_L"], 78244: ["EMG_L", "EMG_R"],  81193: ["EMG_L", "EMG_R"],
 }
