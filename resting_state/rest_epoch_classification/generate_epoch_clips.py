684bcdaa2d20c7fbe0c4363b0952cfb2cbed1410
Olle de Jong <olledejong@gmail.com>
2024-03-26 09:20:38 +0100

    Script now clips the resting-state cluster epochs based on the information held by 'cluster_annotations' in settings.py

--- resting_state/rest_epoch_classification/generate_epoch_clips.py
+++ resting_state/rest_epoch_classification/generate_epoch_clips.py
@@ -7,13 +7,13 @@ import os
 import cv2
 import mne
 import sys
-import pandas as pd
 import numpy as np
-from settings import paths
+import pandas as pd
+from settings import paths, cluster_annotations
 
 
 def generate_clips(subject_epochs, subject_meta, subject_id):
-    print('Generating clips...')
+    print(f'Generating clips for subject {subject_id}...')
 
     # first we need the start and end timepoints of each epoch (in frames)
     epoch_tps_frames = np.array(subject_epochs.metadata["epochs_start_end_frames"])
@@ -23,7 +23,7 @@ def generate_clips(subject_epochs, subject_meta, subject_id):
 
     # now we need to load the video
     video_filename = subject_meta['movie_filename'].iloc[0]
-    path_to_video_file = os.path.join(paths['video_folder'], video_filename)
+    path_to_video_file = os.path.join(paths['recordings_folder'], video_filename)
 
     # open the video and check if it is actually opened
     cap = cv2.VideoCapture(path_to_video_file)
@@ -44,7 +44,10 @@ def generate_clips(subject_epochs, subject_meta, subject_id):
             print(f"Warning: End time for clip {i+1} is beyond video length. Using last frame instead.")
             end_frame = frame_count - 1
 
-        output_file = os.path.join(paths['video_folder'], 'clips', f"{subject_id}_epoch_{epoch_indexes[i]}.mp4")
+        output_file = os.path.join(
+            paths['video_analysis_output'], 'clips',
+            f"{subject_id}_resting_cluster_epoch_{epoch_indexes[i]}.mp4"
+        )
         out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
 
         cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
@@ -69,25 +72,29 @@ def main():
     metadata_df = pd.read_excel(paths["metadata"])
 
     for i, subject_id in enumerate(metadata_df['mouseId']):
+
         subject_meta = metadata_df[metadata_df['mouseId'] == int(subject_id)]
 
-        # load this subject's epochs
-        epoch_folder = os.path.join(paths['epochs_folder'], "ploss_thresh_500")
-        epoch_file = os.path.join(epoch_folder, f'non_mov_epochs_to_classify_{subject_id}-epo.fif')
+        # load this subject's epochs (which include the cluster annotations in the metadata)
+        subject_epochs_path = os.path.join(paths['epochs_folder'], f'filtered_epochs_w_clusters_{subject_id}-epo.fif')
 
-        if not os.path.exists(epoch_file):
-            print(f"No non-movement, non-sleep epoch file for subject {subject_id}, proceeding..")
+        if not os.path.exists(subject_epochs_path):
+            print(f"No epoch file with clustering annotation found for subject {subject_id}, proceeding..")
             continue
 
-        subject_epochs = mne.read_epochs(epoch_file)
+        subject_epochs = mne.read_epochs(subject_epochs_path)
 
-        all_clips = os.listdir(os.path.join(paths['video_folder'], 'clips'))
+        all_clips = os.listdir(os.path.join(paths['video_analysis_output'], 'clips'))
         if any(str(subject_id) in file for file in all_clips):
             print(f"Clips have probably already been generated for subject {subject_id}, proceeding..")
             continue
 
+        # get the resting-state cluster epochs
+        resting_state_cluster_id = cluster_annotations[int(subject_id)]['rest']
+        resting_state_epochs = subject_epochs[subject_epochs.metadata['cluster'] == resting_state_cluster_id]
+
         # generate the movie clips
-        generate_clips(subject_epochs, subject_meta, subject_id)
+        generate_clips(resting_state_epochs, subject_meta, subject_id)
 
         print(f"Subject {subject_id} complete. Total progress: {round( (i+1) / len(metadata_df['mouseId']) * 100)}%.")
     print("Done, bye.")
