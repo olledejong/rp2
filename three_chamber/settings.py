11913b26a67d98fef7236d0f6b1b210968d913a1
Olle de Jong <olledejong@gmail.com>
2024-04-18 10:27:56 +0200

    Dictionary variable -> regular variable

--- three_chamber/settings.py
+++ three_chamber/settings.py
@@ -2,11 +2,7 @@
 Settings specific to the analysis of the 3-chamber experiments
 """
 
-# used in additions to the general EEG filtering settings (see settings_general.py at root of project)
-# used while creating Neurodata Without Border (NWB) files (one of the first steps)
-filtering = {
-    'resample_freq': 500  # set to desired sampling frequency or to None if you do not wish to down-sample the EEG data
-}
+resample_freq = 500  # set to desired sampling frequency or to None if you do not wish to down-sample the EEG data
 
 paths_3c_sociability = {
     "metadata": "/Users/olledejong/Documents/MSc_Biology/ResearchProject2/rp2_data/3C_sociability/output/3c_sociability_metadata.xlsx",
