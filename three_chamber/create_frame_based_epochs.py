bbb678f3eb1da0a464b85fc2455710ef43e991a8
Olle de Jong <olledejong@gmail.com>
2024-04-22 10:57:16 +0200

    Frame-based epoching logic done for 3-chamber sociability experiment. TODO: adjust for 3-chamber preference experiment.

--- three_chamber/create_frame_based_epochs.py
+++ three_chamber/create_frame_based_epochs.py
@@ -1,3 +1,5 @@
+import re
+import mne
 import pickle
 import ndx_events
 import numpy as np
@@ -5,8 +7,9 @@ import pandas as pd
 from pynwb import NWBHDF5IO
 
 from shared.helper_functions import *
-from settings import subject_id_dict, min_interaction_duration
+from settings import subject_id_dict, min_interaction_duration, desired_epoch_length, epoch_overlap_cutoff
 from shared.eeg_video_alignment_functions import adjust_fps, get_first_ttl_offset
+from shared.nwb_retrieval_functions import get_eeg
 
 
 def merge_event_rows(beh_data):
@@ -38,7 +41,9 @@ def get_led_onsets(video_analysis_dir, batch_cage_name):
     with open(os.path.join(video_analysis_dir, 'pickle/led_states.pickle'), "rb") as f:
         led_states = pickle.load(f)
         batch, cage = batch_cage_name.split('_')
-        movie_filename = [filename for filename in led_states.keys() if batch in filename and cage in filename]
+        movie_filename = [
+            fn for fn in led_states.keys() if bool(re.search(f'{batch}_', fn)) and bool(re.search(cage, fn))
+        ]
 
         # if there's a repeat file for this subject, use that instead of the original file
         if len(movie_filename) != 1:
@@ -69,71 +74,145 @@ def frame_to_sample(video_frame, adjusted_fps, offset, s_freq):
     return eeg_tp_secs * s_freq  # go to samples
 
 
-def get_epochs(beh_data_subset, eeg_signal, adjusted_video_fps, offset, s_freq):
+def get_epoch_overlap(event):
+    """
+    Calculates the overlap each fixed epoch of 'desired_epoch_length' needs to have with the previous one in order
+    to capture all data that falls within an interaction.
+
+    Example:
+    To calculate the overlap in seconds we determine how many overlaps there are between the supposed epochs
+    e.g. we have an interaction of duration 2,4 seconds. If we start at 0 and create epochs of 1 second, this
+    would give us 3 epochs, however, the last one isn't of the same length (0.4 seconds)
+    we then get the overlap needed to capture all data within epochs of 1 second by dividing the amount of seconds
+    missing that would have created a full 3rd epoch (0.6 seconds) by the amount of epoch overlaps (3)
+    in this case the overlap is thus 0.2 seconds.
+
+    :param event: the interaction event with start/stop time in frames and interaction duration
+    :return:
+    """
+    total_duration = event['Interaction duration']
+
+    # let's calculate the number of epochs of length 'desired_epoch_length' that fit into the interaction
+    # this is also the number of overlaps between all epochs if you would cut it at increments of
+    # 'desired_epoch_length'
+    num_full_epochs = int(total_duration // desired_epoch_length)
+
+    # get the amount of seconds that would've made the last epoch equal to 'desired_epoch_length'
+    missing_seconds = 1 - (total_duration - num_full_epochs)
+
+    # now, if we divide the 'missing_seconds' by the 'num_full_epochs' (i.e. amount of epoch overlaps), we get the
+    # needed amount of seconds each epoch has to overlap with the previous one to capture all data
+    overlap = missing_seconds / num_full_epochs
+
+    return overlap
+
+
+def get_epochs(nwb_file_path, beh_data_subset, adjusted_video_fps, offset, s_freq):
     """
     Generates frame based epochs using the frame-timestamped recorded behaviours in 'beh_data_subset'.
     We use the adjusted FPS and the offset between the two first TTL onsets to go from the frame timestamps
     (start/stop) to the correct (start/stop) sample in the EEG.
 
+    :param nwb_file_path:
     :param beh_data_subset:
-    :param eeg_signal:
     :param adjusted_video_fps:
     :param offset:
     :param s_freq:
     :return:
     """
-    raw_epochs, filtered_epochs = [], []
+    print('\n')
+    all_interaction_epochs = []
 
+    n_skipped = 0
+    # loop through all events
     for index, event in beh_data_subset.iterrows():
 
+        # get the start and stop frame time-point of this event
         start_frame, stop_frame = int(event['Frame start']), int(event['Frame stop'])
-        print(start_frame, stop_frame)
-        start_sample = np.floor(frame_to_sample(start_frame, adjusted_video_fps, offset, s_freq))
-        stop_sample = np.ceil(frame_to_sample(stop_frame, adjusted_video_fps, offset, s_freq))
-        print(start_sample, stop_sample)
 
-    return raw_epochs, filtered_epochs
+        # using the adjusted FPS and the offset of the first TTL, get the start/stop time-points of the event in samples
+        interaction_start = int(np.floor(frame_to_sample(start_frame, adjusted_video_fps, offset, s_freq)))
+        interaction_end = int(np.ceil(frame_to_sample(stop_frame, adjusted_video_fps, offset, s_freq)))
+
+        interaction_eeg, chans = get_eeg(nwb_file_path, 'filtered_EEG', (interaction_start, interaction_end), True)
+        print(chans)
+        # get overlap each epoch has to have with the previous one in order to capture all EEG data in epochs of 1 sec
+        overlap = get_epoch_overlap(event)
+
+        # we check if the 'epoch_overlap_cutoff * desired_epoch_length', which is the duration of the epoch length that
+        # we allow as overlap, is larger than the calculated overlap needed to capture all data.
+        max_allowed_overlap = epoch_overlap_cutoff * desired_epoch_length  # in seconds
+        if overlap > max_allowed_overlap:
+            print(f'Interaction {index} skipped due to overlap of {overlap:.2f}s on {desired_epoch_length}s epochs.'
+                  f' Exceeds max {epoch_overlap_cutoff * 100:.1f}% of overlap')
+            overlap = 0.0
+            n_skipped += 1
+
+        ch_types = ["emg" if "EMG" in chan else "eeg" for chan in chans]
+        info = mne.create_info(ch_names=list(chans), sfreq=s_freq, ch_types=ch_types)
+        raw = mne.io.RawArray(interaction_eeg, info)
+
+        # make fixed length epochs of 'desired_epoch_length' length
+        epochs = mne.make_fixed_length_epochs(
+            raw, duration=desired_epoch_length, overlap=overlap, preload=True
+        )
+
+        # create metadata dataframe and add to epochs array
+        metadata = pd.DataFrame({
+            'interaction_n': [index + 1] * len(epochs),
+            'interaction_part_n': range(1, len(epochs) + 1),
+            'interaction_kind': [event["Behavior"]] * len(epochs),
+            'total_interaction_duration': [event["Interaction duration"]] * len(epochs),
+            'epoch_length': [desired_epoch_length] * len(epochs),
+        })
+        epochs.metadata = metadata
+
+        # save this interaction's epochs
+        all_interaction_epochs.append(epochs)
+
+    print(f'\n{n_skipped} out of {len(beh_data_subset)} skipped due to size of overlap.')
+
+    # concatenate all epoch arrays
+    all_epochs = mne.concatenate_epochs(all_interaction_epochs)
+
+    return all_epochs
 
 
 def main():
-    # nwb_folder = select_folder("Select the folder holding your 3-chamber experiment NWB files")
-    # experiment_metadata = select_file("Select the experiment's metadata file")
-    # behaviour_data = select_folder("Select the experiment's behaviour data folder")
-    # video_analysis_folder = select_folder("Select the folder that holds the video analysis output (ROI df, pickle folder, etc)")
-
-    nwb_folder = "/Users/olledejong/Documents/MSc_Biology/ResearchProject2/rp2_data/3C_sociability/output/nwb"
-    experiment_metadata = "/Users/olledejong/Documents/MSc_Biology/ResearchProject2/rp2_data/3C_sociability/output/3c_sociability_metadata.xlsx"
-    behaviour_data = "/Users/olledejong/Documents/MSc_Biology/ResearchProject2/rp2_data/3C_sociability/input/behavioural_data"
-    video_analysis_folder = "/Users/olledejong/Documents/MSc_Biology/ResearchProject2/rp2_data/3C_sociability/output/videos"
-
-    experiment_meta = pd.read_excel(experiment_metadata)
+    nwb_folder = select_folder("Select the folder holding your 3-chamber experiment NWB files")
+    behaviour_data = select_folder("Select the experiment's behaviour data folder")
+    video_analysis_folder = select_folder("Select the folder that holds the video analysis output (ROI df, pickle folder, etc)")
+    epochs_folder = select_or_create_folder("Select or create a folder to where the epoch files are saved")
 
     # create a raw and filtered epochs file per subject (nwb)
     for file in sorted(os.listdir(nwb_folder)):
         if not file.endswith(".nwb"):
             continue
 
-        with NWBHDF5IO(os.path.join(nwb_folder, file), "r") as io:
+        nwb_file_path = os.path.join(nwb_folder, file)
+        with NWBHDF5IO(nwb_file_path, "r") as io:
             nwb = io.read()
 
-            filtered_eeg = nwb.acquisition['filtered_EEG'].data[:].T
-            s_freq = nwb.acquisition['filtered_EEG'].rate  # sampling frequency of the EEG
-            eeg_ttl_onsets_secs = list(nwb.acquisition["TTL_1"].timestamps)
+            filtered_eeg = nwb.acquisition['filtered_EEG'].data[:].T[0]  # only one channel needed to adjust the fps
+            s_freq = nwb.acquisition['filtered_EEG'].rate  # sampling frequency/resampled frequency of the EEG
+            eeg_ttl_onsets_secs = list(nwb.acquisition["TTL_1"].timestamps)  # timestamps of the TTL onsets in seconds
             subject_id = nwb.subject.subject_id  # subject id
+            io.close()
 
         # get the batch_cage combination name to retrieve the correct behaviour data
         batch_cage = [key for key, value in subject_id_dict.items() if value == int(subject_id)][0]
-        print(f'Getting {batch_cage}.xlsx file belonging to subject {subject_id}')
+        print(f'\nGetting {batch_cage}.xlsx file belonging to subject {subject_id}')
 
         # load the behavioural data and then merge start/stop events
         # tracking data from BORIS software has 2 rows for each state event (start/stop), we want one for each
         beh_data = pd.read_excel(os.path.join(behaviour_data, f'{batch_cage}.xlsx'))
         beh_data = merge_event_rows(beh_data)
 
-        print(f'Number of durations longer than {min_interaction_duration} seconds:'
+        print(f'Number of interactions longer than {min_interaction_duration} seconds:'
               f' {len(beh_data[beh_data["Interaction duration"] > min_interaction_duration])}\n')
 
         beh_data_subset = beh_data[beh_data["Interaction duration"] > min_interaction_duration]
+        beh_data_subset.reset_index(drop=True, inplace=True)
 
         # get the LED states for this subject (i.e. get the LED states of the correct video)
         # and then get the frames where the LED turned ON (i.e. get all boolean event changes from OFF to ON (0 to 1)
@@ -141,13 +220,17 @@ def main():
 
         # as the video isn't recorded at exactly 30 fps, we calculate the true fps
         # the offset here is the difference in TTL onset between the EEG and LED (negative means that LED has delay)
-        adjusted_fps = adjust_fps(filtered_eeg[0], eeg_ttl_onsets_secs, led_onsets, s_freq)
+        adjusted_fps = adjust_fps(filtered_eeg, eeg_ttl_onsets_secs, led_onsets, s_freq)
         first_ttl_offset = get_first_ttl_offset(eeg_ttl_onsets_secs, led_onsets, adjusted_fps, s_freq)
 
-        raw_epochs, filtered_epochs = get_epochs(beh_data_subset, filtered_eeg[0], adjusted_fps, first_ttl_offset, s_freq)
-        break
+        # generate fixed length epochs
+        all_epochs = get_epochs(nwb_file_path, beh_data_subset, adjusted_fps, first_ttl_offset, s_freq)
+
+        # save this subject's epochs
+        all_epochs.save(os.path.join(epochs_folder, f'epochs_{subject_id}-epo.fif'), overwrite=True)
+        print(f'\nSuccessfully created and saved {len(all_epochs)} epochs for subject {subject_id}.')
 
 
 if __name__ == '__main__':
     main()
-    print('Done')
+    print('\nDone!')
