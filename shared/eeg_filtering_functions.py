7b8288f95567d0069ab3ffebd7a2b741192c0871
Olle de Jong <olledejong@gmail.com>
2024-05-13 15:52:32 +0200

    User can now choose whether to use scipy or mne filtering of raw EEG data

--- shared/eeg_filtering_functions.py
+++ shared/eeg_filtering_functions.py
@@ -1,6 +1,9 @@
 """
 Holds functions that are utilized to filter the raw EEG signal
 """
+import sys
+
+import mne
 import numpy as np
 from scipy import signal
 from scipy.interpolate import interp1d
@@ -27,7 +30,7 @@ def interpolate_nan(padata, pkind='linear'):
 
 # Define general functions
 # noinspection PyTupleAssignmentBalance
-def filter_eeg(x, s_freq, lcut=0.5, hcut=200, lower_val=0.006, higher_val=0.013, art=3):
+def filter_eeg(x, method, s_freq, lcut=0.5, hcut=200, lower_val=0.006, higher_val=0.013, art=3):
     """
     Filters the EEG signal.
 
@@ -39,6 +42,7 @@ def filter_eeg(x, s_freq, lcut=0.5, hcut=200, lower_val=0.006, higher_val=0.013,
     'art' is the multiple of the std at which signal is considered an artifact.
 
     Returns filtered EEG signal
+    :param method: determines if we use scipy's butter filter or mne's filtering function
     :param x: unfiltered data
     :param s_freq: sampling frequency
     :param lcut: lower limit of desired band / filter (to be normalized)
@@ -54,9 +58,17 @@ def filter_eeg(x, s_freq, lcut=0.5, hcut=200, lower_val=0.006, higher_val=0.013,
     rej = np.where(rej < higher_val, rej, np.nan)
     rej = interpolate_nan(rej, pkind='linear')
 
-    # filter by applying a 5th Order Bandpass Butterworth Filter
-    b, a = signal.butter(N=5, Wn=[lcut / (s_freq / 2), hcut / (s_freq / 2)], btype='bandpass')
-    rej = signal.filtfilt(b, a, rej)
+    if method == 'mne':
+        # filter using mne filtering function
+        rej = mne.filter.filter_data(rej, l_freq=lcut, h_freq=hcut, sfreq=s_freq)
+    elif method == 'scipy':
+        # filter by applying a 5th Order Bandpass Butterworth Filter
+        # Wn holds critical frequency or frequencies for lowpass and highpass filters
+        # We normalize the lcut and hcut by dividing them by the Nyquist frequency ( s_freq / 2 )
+        b, a = signal.butter(N=5, Wn=[lcut / (s_freq / 2), hcut / (s_freq / 2)], btype='bandpass')
+        rej = signal.filtfilt(b, a, rej)
+    else:
+        sys.exit('That EEG filtering method does not exist; please choose between mne and scipy.')
 
     # if art is provided as argument (int), perform artifact rejection based on art * std
     if art:
